280d81391d071c05928c98d783d70b9183696f56
Ничипоренко <julianichi@mail.ru>
2025-05-14 22:54:50 +0700

    Книги

---
+++ lab6/book.js
@@ -0,0 +1,82 @@
+const books = [
+        {
+            author: 'Николас Спаркс',
+            name: 'Дневник памяти',
+            publicationYear: 1996,
+            price: 700,
+            bestseller: true,
+            inStock: true
+        },
+        {
+            author: 'Джоан Роулинг',
+            name: 'Гарри Поттер и Философский камень',
+            publicationYear: 1997,
+            price: 1000,
+            bestseller: true,
+            inStock: true
+        },
+        {
+            author: 'Джейн Остин',
+            name: 'Гордость и предубеждение',
+            publicationYear: 1813,
+            price: 600,
+            bestseller: true,
+            inStock: false
+        },
+        {
+            author: 'Джордж Оруэлл',
+            name: '1984',
+            publicationYear: 1949,
+            price: 580,
+            bestseller: true,
+            inStock: true
+        },
+        {
+            author: 'Фрэнсис Скотт Фицджеральд',
+            name: 'Великий Гэтсби',
+            publicationYear: 1925,
+            price: 490,
+            bestseller: true,
+            inStock: true
+        },
+        {
+            author: 'Харпер Ли',
+            name: 'Убить пересмешника',
+            publicationYear: 1960,
+            price: 560,
+            bestseller: true,
+            inStock: false
+        },
+        {
+            author: 'Габриэль Гарсиа Маркес',
+            name: 'Сто лет одиночества',
+            publicationYear: 1967,
+            price: 630,
+            bestseller: true,
+            inStock: true
+        },
+        {
+            author: 'Антуан де Сент-Экзюпери',
+            name: 'Маленький принц',
+            publicationYear: 1943,
+            price: 340,
+            bestseller: false,
+            inStock: true
+        },
+        {
+            author: 'Рэй Брэдбери',
+            name: '451 градус по Фаренгейту',
+            publicationYear: 1953,
+            price: 480,
+            bestseller: true,
+            inStock: true
+        },
+        {
+            author: 'Лев Толстой',
+            name: 'Война и мир',
+            publicationYear: 1869,
+            price: 990,
+            bestseller: false,
+            inStock: false
+        }
+    ];
